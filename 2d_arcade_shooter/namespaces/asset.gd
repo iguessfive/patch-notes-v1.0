84958b457884cab92ae31d7a14491d63f1eb7376
iguessfive <iguessfive@outlook.com>
2025-09-14 10:36:57 +0100

    removed git file for 2d

--- 2d_arcade_shooter/namespaces/asset.gd
+++
@@ -1,4 +0,0 @@
-class_name Asset extends Resource
-
-class Visual extends Asset:
-	const white_1x1 = preload("res://assets/white_1x1.png")
--- 2d_arcade_shooter/scripts/mob_resource.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-
-@export var speed_movement: float = 100.0
-@export var health: int = 1
