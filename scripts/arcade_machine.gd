3b199b5e677f754ec2d56bce58a9c7fddf5d0d84
iguessfive <iguessfive@outlook.com>
2025-09-14 18:48:40 +0100

    added key binding to enter 2d shooter

--- scripts/arcade_machine.gd
+++ scripts/arcade_machine.gd
@@ -5,10 +5,29 @@ extends StaticBody3D
 @onready var screen_mesh: MeshInstance3D = $Model/ScreenMesh
 @onready var interact_area: Area3D =  $InteractArea
 
+var is_within_interact_area: bool = false
+
 func _ready() -> void:
+	InputMap.add_action("enter_arcade")
+	var event := InputEventKey.new()
+	event.physical_keycode = KEY_E
+	InputMap.action_add_event("enter_arcade", event)
+
 	interact_area.body_entered.connect(func(body: Node) -> void:
 		if body is Type.Player3D:
-			get_tree().change_scene_to_file.call_deferred(shooter_2d_path)
-			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
+			is_within_interact_area = true
+			$Label3D.show()
 	)
+
+	interact_area.body_exited.connect(func(body: Node) -> void:
+		if body is Type.Player3D:
+			is_within_interact_area = false
+			$Label3D.hide()
+	)
+
+@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
+	if is_within_interact_area and Input.is_action_pressed("enter_arcade"):
+		print(is_within_interact_area)
+		get_tree().change_scene_to_file.call_deferred(shooter_2d_path)
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
